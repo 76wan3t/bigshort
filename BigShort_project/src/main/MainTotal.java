df8258af7d61d27f26784a3c583b4dc99f4425d0
kdh6335 <chakim6@gmail.com>
2018-03-24 21:41:12 +0900

    코드 추가 및 수정 구 끼리 한개로 통일 함

---
+++ BigShort_project/src/main/MainTotal.java
@@ -0,0 +1,75 @@
+package main;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import bukgu.BukGuTotal;
+import donggu.DongGuTotal;
+import gwangsan.GwangSanGuTotal;
+import namgu.NamGuTotal;
+import seogu.SeoGuTotal;
+
+public class MainTotal {
+      
+      
+      public static void main(String[] args) throws IOException {
+            
+            Scanner sc = new Scanner(System.in);
+            
+             System.out.println("광주광역시 사랑방 클로링 프로그램");
+              System.out.println("1. 광산구");
+              System.out.println("2. 북구");
+              System.out.println("3. 동구");
+              System.out.println("4. 서구");
+              System.out.println("5. 남구");
+              System.out.println("6. 프로그램 종료");
+              System.out.print("code : ");
+              int code = sc.nextInt();
+             
+              if(code == 1) {
+                  
+                  System.out.println("광산구 지역입니다.");
+                  GwangSanGuTotal gSgt = new GwangSanGuTotal();
+                  gSgt.gwangsangutotal();
+                  
+                  
+              }else if(code == 2) {
+                  
+                  System.out.println("북구 지역입니다.");
+                  BukGuTotal bGt = new BukGuTotal();
+                  bGt.bukgutotal();
+                  
+              }else if(code == 3) {
+                                    
+                  System.out.println("동구 지역입니다.");
+                  
+                  DongGuTotal dGu = new DongGuTotal();
+                  dGu.donggutotal();
+                  
+              }else if(code == 4) {
+                  
+                  System.out.println("남구 지역입니다.");
+                  
+                  NamGuTotal nGt = new NamGuTotal();
+                  nGt.namgutotal();
+                  
+              }else if(code == 5) {
+                  
+                  System.out.println("서구 지역입니다.");
+                  
+                  SeoGuTotal sGt = new SeoGuTotal();
+                  sGt.namgutotal();
+                  
+              }else if(code == 6) {
+                  
+                  System.exit(0);
+                  
+              }else{
+                     System.out.println("잘못 입력 하셨습니다.");
+                    
+                    }
+             
+      }
+      
+
+}
---
+++ BigShort_project/src/seogu/SeoGu.java
@@ -0,0 +1,5 @@
+package seogu;
+
+public class SeoGu {
+
+}
